0c4d22cb4479de3a3a127884a17d562e29c12567
oaugustoreis <eu.oaugustoreis@gmail.com>
2024-09-01 18:29:56 -0400

    Fiz aqui

---
+++ newsio/views.py
@@ -0,0 +1,80 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.contrib.auth import authenticate, logout, login
+from django.urls import reverse
+import requests, json
+from .models import Category
+
+def index(request):
+    category = Category.objects.all()
+    results=get_news(fromDate=2020-2-22, toDate=2024-2-26, topic='futebol')
+    return render(request, "layout.html",{
+        "results": results,
+        "category": category
+    })
+
+
+def register_view(request):
+    if request.method == "POST":
+       username = request.POST['username']
+       email = request.POST['email'] 
+       user = User.objects.filter(username=username).first()
+       if user:
+           return HttpResponse("Ja existe") 
+       user = User.objects.create(username=username, email=email)
+       user.save()
+       return HttpResponseRedirect(reverse("index")) 
+    else:
+       return render(request, 'register.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def login_view(request):
+    if request.method == "POST":
+       username = request.POST['username']
+       password = request.POST['password'] 
+
+       user = authenticate(username=username, password=password)
+
+       if user:
+          login(request, user)
+          return HttpResponseRedirect(reverse("index"))
+    else:
+       return render(request, 'login.html')
+      
+
+def get_news(fromDate, toDate, topic):
+    url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={fromDate}&to={toDate}&sortBy=popularity&language=pt&apiKey=e457aaaca1c7408fa60f069dd7f27148'
+    r = requests.get(url)
+    data = r.json()
+    results=[]
+    articles = data['articles']
+    for article in articles:
+        result = {
+            'title': article['title'],
+            'description': article['description'],
+        }
+        results.append(result)
+    return results
+
+
+def search_view(request):
+    if request.method == "POST":
+        inTitle = request.POST['inTitle']
+        fromDate=request.POST['fromDate']
+        toDate=request.POST['toDate']
+        results=get_news(fromDate=fromDate, toDate=toDate, topic='corrida')
+        return render(request, 'search.html', {
+            "inTitle": inTitle,
+            "fromDate": fromDate,
+            "toDate": toDate,
+            "result": results,
+            "category": Category.objects.all()
+        })
+    else:
+         return HttpResponseRedirect(reverse("index")) 
+        
