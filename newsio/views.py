a0d59258c2ff51da748e6ee4963ae25d188008f7
oaugustoreis <eu.oaugustoreis@gmail.com>
2024-09-02 22:25:11 -0400

    index img estilo

--- newsio/views.py
+++ newsio/views.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, logout, login
 from django.urls import reverse
-import requests, json
+import requests
 from .models import Category
 
 def index(request):
     category = Category.objects.all()
-    results=get_news(fromDate=2020-2-22, toDate=2024-2-26, topic='futebol')
+    results=get_news(fromDate=None, toDate=None, topic=None, category=None, indexView=True)
     return render(request, "layout.html",{
         "results": results,
         "category": category
@@ -47,8 +47,12 @@ def login_view(request):
        return render(request, 'login.html')
       
 
-def get_news(fromDate, toDate, topic):
-    url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={fromDate}&to={toDate}&sortBy=popularity&language=pt&apiKey=e457aaaca1c7408fa60f069dd7f27148'
+def get_news(fromDate, toDate, topic,category, indexView):
+    url = ""
+    if indexView==True:
+        url = f'https://newsapi.org/v2/everything?q=world&sortBy=popularity&language=en&apiKey=e457aaaca1c7408fa60f069dd7f27148'
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?q={topic}&category={category}&from={fromDate}&to={toDate}&sortBy=popularity&language=en&apiKey=e457aaaca1c7408fa60f069dd7f27148'
     r = requests.get(url)
     data = r.json()
     results=[]
@@ -57,6 +61,8 @@ def get_news(fromDate, toDate, topic):
         result = {
             'title': article['title'],
             'description': article['description'],
+            'url': article['url'],
+            'urlimage': article['urlToImage'],
         }
         results.append(result)
     return results
@@ -65,9 +71,10 @@ def get_news(fromDate, toDate, topic):
 def search_view(request):
     if request.method == "POST":
         inTitle = request.POST['inTitle']
+        category = request.POST['category']
         fromDate=request.POST['fromDate']
         toDate=request.POST['toDate']
-        results=get_news(fromDate=fromDate, toDate=toDate, topic='corrida')
+        results=get_news(fromDate=fromDate, toDate=toDate, topic=inTitle,category=category, indexView=False)
         return render(request, 'search.html', {
             "inTitle": inTitle,
             "fromDate": fromDate,
